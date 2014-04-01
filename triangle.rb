18be889e00eb2ce7963e5eec47efa7e4d899f32d
joeytall <joeytall.liu@gmail.com>
2014-04-01 08:41:28 -0700

    update prograss
    Change-Id: I4af513e0468c5ddcaeeb6adad187469eefd3d24a

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,9 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  a,b,c = [a,b,c].sort
+  raise TriangleError if a <= 0 || a + b <= c
+
   case [a,b,c].uniq.size
   when 1 then :equilateral
   when 2 then :isosceles
